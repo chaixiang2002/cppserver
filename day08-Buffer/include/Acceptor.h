9a55648f69745acbb7b4e8a8ce8cf8aa8089ea72
chaixiang <973731820@qq.com>
2023-10-06 20:26:16 +0800

    新增缓冲区 Buffer类，读缓存和写缓存

---
+++ day08-Buffer/include/Acceptor.h
@@ -0,0 +1,33 @@
+#pragma once
+#include <functional>
+class EventLoop;
+class Socket;
+class InetAddress;
+class Channel;
+
+/**
+ * new sockert
+ * new addr
+ * new channel
+ * 绑定现有的loop
+ * 
+ * std::function<void(Socket*)> newConnectionCallback；
+ * setNewConnectionCallback()设置setNewConnectionCallback
+*/
+class Acceptor
+{
+private:
+    EventLoop *loop;
+    Socket *sock;
+    // InetAddress *addr;
+    Channel *acceptChannel;
+    std::function<void(Socket*)> newConnectCallback;
+
+public:
+    Acceptor(EventLoop *_loop);
+    ~Acceptor();
+    void acceptConnection();
+    // std::function<void(Socket*)> newConnectCallback;
+    void setNewConnectionCallback(std::function<void(Socket*)>);
+};
+
